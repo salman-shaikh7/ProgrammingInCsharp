5eadb11dc62732eee0133211169189a90875d4d2
harshasaijagu <harshasaijagu@usf.edu>
2024-09-25 18:13:11 -0400

    Modified Program.cs

--- Program.cs
+++ Program.cs
@@ -6,8 +6,44 @@ namespace ProgrammingInCSharp
     {
         static void Main(string[] args)
         {
-            // Call the RunExample method from BasicSyntax to execute its code
+            // Introduction to Programming
+            Console.WriteLine("Running Introduction to Programming (Basic Syntax)...");
             IntroductionToProgramming.VariablesAndDataTypes.RunExample();
+            // IntroductionToProgramming.ControlStructures.RunExample();
+            // IntroductionToProgramming.BasicSyntax.RunExample();
+
+            // Data Structures
+            // Console.WriteLine("Running Data Structures...");
+            // DataStructures.Arrays.ArraysBasics.RunExample();
+            // DataStructures.Strings.StringManipulation.RunExample();
+            // DataStructures.Dictionaries.DictionaryOperations.RunExample();
+
+            // Sorting Algorithms
+            // Console.WriteLine("Running Sorting Algorithms...");
+            // SortingAlgorithms.BubbleSort.RunExample();
+            // SortingAlgorithms.QuickSort.RunExample();
+            // SortingAlgorithms.MergeSort.RunExample();
+
+            // Searching Algorithms
+            // Console.WriteLine("Running Searching Algorithms...");
+            // SearchingAlgorithms.BinarySearch.RunExample();
+            // SearchingAlgorithms.LinearSearch.RunExample();
+
+            // Big O Notation
+            // Console.WriteLine("Running Big O Notation Examples...");
+            // BigONotation.BigOExamples.RunExample();
+
+            // Algorithms (Recursion, Divide and Conquer, Dynamic Programming)
+            // Console.WriteLine("Running Algorithms...");
+            // Algorithms.Recursion.Fibonacci.RunExample();
+            // Algorithms.DivideAndConquer.MergeSort.RunExample();
+            // Algorithms.DynamicProgramming.KnapsackProblem.RunExample();
+
+            // Object-Oriented Programming (OOP)
+            // Console.WriteLine("Running Object-Oriented Programming Examples...");
+            // OOPs.ClassesAndObjects.RunExample();
+            // OOPs.InheritanceExample.RunExample();
+            // OOPs.PolymorphismExample.RunExample();
 
             // Program execution completed
             Console.WriteLine("\nProgram execution completed.");
